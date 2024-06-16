c5ed6caf0381be2ccf7dfcce4b7fccdca4d463cf
Vijaybabu26 <155373404+Vijaybabu26@users.noreply.github.com>
2024-06-16 11:46:50 +0530

    Delete FoodDeliveryAppBackEnd directory

--- FoodDeliveryAppBackEnd/src/main/java/com/babu/fooddelivery/repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.babu.fooddelivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.babu.fooddelivery.dto.Order;
-
-public interface OrderRepo extends JpaRepository<Order, Integer>{
-	
-}
