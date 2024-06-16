f61e8d143989f5f93a31f26f704a42a42302afba
Vijaybabu26 <155373404+Vijaybabu26@users.noreply.github.com>
2024-06-16 11:46:33 +0530

    Delete FoodDelivery directory

--- FoodDelivery/src/main/java/com/babu/fooddelivery/repo/RestaurantRepo.java
+++
@@ -1,9 +0,0 @@
-package com.babu.fooddelivery.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.babu.fooddelivery.dto.Restaurant;
-
-public interface RestaurantRepo extends JpaRepository<Restaurant,Long>{
-
-}
--- FoodDelivery/src/main/java/com/babu/fooddelivery/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.babu.fooddelivery.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.babu.fooddelivery.dto.User;
-
-public interface UserRepo extends JpaRepository<User,Long>{
-	
-}
